e87d73bb635e2b435fa801e391da1f25585d47f5
martogasha <m.gachanga97@gmail.com>
2025-11-10 12:15:09 +0300

    first codas

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -10,6 +10,8 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use RouterOS\Client;
+use RouterOS\Query;
 use RouterOS\Config;
 
 class MpesaController extends Controller
