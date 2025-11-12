a64a6d154b7a21dd6bc3964e1172fa179a545f5b
martogasha <m.gachanga97@gmail.com>
2025-11-12 20:05:21 +0300

    first codassdvsdffbsf

--- app/Console/Commands/sendSms.php
+++ app/Console/Commands/sendSms.php
@@ -3,6 +3,31 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Cat;
+use App\Exceptions\Controller;
+use App\Models\Cash;
+use App\Models\Expense;
+use App\Models\Inv;
+use App\Models\Invoice;
+use App\Models\Mpesa;
+use App\Models\Notice;
+use App\Models\Payment;
+use App\Models\Profile;
+use App\Models\Product;
+use App\Models\Qproduct;
+use App\Models\Quotation;
+use App\Models\User;
+use Carbon\Carbon;
+use Dompdf\Dompdf;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Session;
+use RouterOS\Client;
+use RouterOS\Query;
+use RouterOS\Config;
+use Illuminate\Support\Facades\Log; 
+use Illuminate\Support\Facades\Http;
 
 class sendSms extends Command
 {
@@ -37,6 +62,44 @@ class sendSms extends Command
      */
     public function handle()
     {
-        return 0;
+          $gets = User::where('role',2)->get();
+        foreach($gets as $get ){
+            $getTwoDayDate =  Invoice::where('user_id',$get->id)->latest('id')->first();
+            if($getTwoDayDate->status ==1){
+                $twoDays = $getTwoDayDate->two_days_before;
+               
+                if($twoDays = Carbon::now()){  
+                    $postData = [
+                        'apikey' => '04be700f6000ae7ec7c7b7e75d7f0f52',
+                        'partnerID' => 15,
+                        'mobile' => $get->phoneOne,
+                        'message' => 'Dear customer, your DOLEX subscription is due for renewal on '.date('d/m/Y',strtotime($get->due_date)).'. Pay to avoid disconnection. PAYBILL: 6589582 ACC NO: '.$get->phone.'',
+                        'shortcode' => 'DOLEX TECH',
+                        
+                    ];
+                    $respons = Http::post('https://sms.imarabiz.com/api/services/sendsms/', $postData);
+                }
+            }
+          
+                $getOneDayDate =  Invoice::where('user_id',$get->id)->latest('id')->first();
+                if($getOneDayDate->status==1){
+                    $oneDay = $getOneDayDate->one_day_before;
+                    
+                
+                    if($oneDay = Carbon::now()){
+                        
+                        $postData = [
+                            'apikey' => '04be700f6000ae7ec7c7b7e75d7f0f52',
+                            'partnerID' => 15,
+                            'mobile' => $get->phoneOne,
+                            'message' => 'Dear customer, your DOLEX subscription is due for renewal on '.date('d/m/Y',strtotime($get->due_date)).'. Pay to avoid disconnection. PAYBILL: 6589582 ACC NO: '.$get->phone.'',
+                            'shortcode' => 'DOLEX TECH',
+                            
+                        ];
+                        $respons = Http::post('https://sms.imarabiz.com/api/services/sendsms/', $postData);
+                    }
+                }
+               
+        }
     }
 }
