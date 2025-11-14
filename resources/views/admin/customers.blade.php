32aa80d63b868cbe38bfadd740218a6899dadda1
martogasha <m.gachanga97@gmail.com>
2025-11-14 11:53:09 +0300

    first codasdsdsghdgfhshrtjtrd

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -90,7 +90,10 @@
                                         @else
                                         @if(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('two_days_before')>App\Models\User::where('id',$customer->id)->value('due_date'))
                                          <td>{{date('d/m/Y',strtotime(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('two_days_before')))}}</td>
+                                         @else
+                                         <td><span class="badge badge-success">Msg Sent</span></td></td>
                                          @endif
+                                        
                                         <td>{{date('d/m/Y',strtotime(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('one_day_before')))}}</td>
                                         @endif
                                     <td>
