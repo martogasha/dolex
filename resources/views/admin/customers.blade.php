f19780b071a2f1605edcb55299fd242a3993ed88
martogasha <m.gachanga97@gmail.com>
2025-11-14 11:56:08 +0300

    first codasdsdsghdgfhshrtjtr

--- resources/views/admin/customers.blade.php
+++ resources/views/admin/customers.blade.php
@@ -88,7 +88,7 @@
                                         <td><span class="badge badge-danger">Disconnected</span></td>
                                         <td><span class="badge badge-danger">Disconnected</span></td>
                                         @else
-                                        @if(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('two_days_before')>App\Models\User::where('id',$customer->id)->value('due_date'))
+                                        @if(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('two_days_before') < App\Models\User::where('id',$customer->id)->value('due_date'))
                                          <td>{{date('d/m/Y',strtotime(App\Models\Invoice::where('user_id',$customer->id)->latest('id')->value('two_days_before')))}}</td>
                                          @else
                                          <td><span class="badge badge-success">Msg Sent</span></td></td>
