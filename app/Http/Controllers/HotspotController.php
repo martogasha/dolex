18e61a32c8febe67552810c6ae6f5c2a462c4940
martogasha <m.gachanga97@gmail.com>
2026-06-19 14:37:13 +0300

    firstvsdva

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class HotspotController extends Controller
 {
-        public function hotspot(Request $request): JsonResponse
+        public function hotspot(Request $request)
     {
         // 1. Process your logic here (e.g., fetch database records or compute information)
    
