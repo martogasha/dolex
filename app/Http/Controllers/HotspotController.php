c168b8d86b6df4e673a4a7d2a7fa51cc4126a25d
martogasha <m.gachanga97@gmail.com>
2026-06-19 14:36:18 +0300

    firstvsdvas

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -10,11 +10,7 @@ class HotspotController extends Controller
         public function hotspot(Request $request): JsonResponse
     {
         // 1. Process your logic here (e.g., fetch database records or compute information)
-        $data = [
-            'status' => 'success',
-            'message' => 'Function initiated successfully!',
-            'timestamp' => now()
-        ];
+   
 
         // 2. Return the response as JSON
         dd('ok');
