61b58f4db643e3c197b9650c90be3e99c83fc4b2
martogasha <m.gachanga97@gmail.com>
2026-09-01 14:46:35 +0300

    firstvsdvdckjgk

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -93,7 +93,7 @@ class MpesaController extends Controller
                         try {
                         // 2. Initialize the MikroTik API Client
                         $client = new Client([
-                            'host' => 'http:e7e90fa2420f.sn.mynetname.net',
+                            'host' => 'e7e90fa2420f.sn.mynetname.net',
                             'user' => 'admin',
                             'pass' => '123456',
                             'port' => 8728,
