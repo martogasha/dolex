3d63109f0e319efd8309ec45d3e8d2c46a5119ed
martogasha <m.gachanga97@gmail.com>
2026-09-26 15:29:29 +0300

    Initialcommddcdcd

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -129,6 +129,8 @@ class MpesaController extends Controller
                         $query = new Query('/ip/hotspot/user/add');
                         $query->equal('name', $getHotspot->phone);
                         $query->equal('password', $getHotspot->phone);
+                        $query->equal('ip_address', $getHotspot->ip);
+                        $query->equal('mac_address', $getHotspot->mac);
                         
                         if (!empty($validated['profile'])) {
                             $query->equal('profile', $validated['profile']);
